abf86f76d9c0fe286c50df226d2b44dc848ce5b6
Curran Robertson <cjr5480@gmail.com>
2024-01-19 12:15:21 -0700

    update

--- divergence.cpp
+++ divergence.cpp
@@ -8,27 +8,24 @@ using namespace std;
 typedef std::function<double(double)> RealFunc;
 typedef std::function<double(std::function<double(double)>, double)> RealFuncDerivative;
 
-double electricField(double x, double y, double z) {
-    vector<double> E = {2 * x, y, z};
-    return E;
-}
+std::vector<double(double x, double y, double z)> electricField(2 * x, 3 * y, 4 * z);
 
 double partialDerivatives(RealFunc f, double x, double y, double z) {
     double h = sqrt(std::numeric_limits<double>::epsilon());
     double delfx = (f(x + h) - f(x - h)) / (2.0 * h);
-    return delfx;
 
     double delfy = (f(y + h) - f(y - h)) / (2.0 * h);
-    return delfy;
 
     double delfz = (f(z + h) - f(z - h)) / (2.0 * h);
-    return delfz;
+    return delfx, delfy, delfz;
 }
 
 double divergence(double delfx, double delfy, double delfz) {
     double divF = delfx + delfy + delfz;
 }
 
-int main(double divF) {
+int main() {
+    RealFunc f{electricField }
+    
     cout << "Divergence of Electric Field: " << divF << endl;
 }
---
+++ localDivergence.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <limits>
+#include <cmath>
+#include <divergence.cpp>
+using namespace std;
+
+int main(double divF) {
+    cout << "Divergence of Electric Field: " << divF << endl;
+}
